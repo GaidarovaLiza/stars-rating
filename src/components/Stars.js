0848cbd39612616192781815da5eecb1ba21e064
Elizaveta Gaidarova <gaidarova.liza@gmail.com>
2022-04-11 11:18:34 +0300

    First commit

---
+++ src/components/Stars.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Star from './Star';
+
+export default function Stars(props) {
+	return (
+		<ul className="card-body-stars u-clearfix">
+			<li>
+				{props.count <= 0 || props.count > 5 || typeof props.count === 'string'
+					? null
+					:
+					[...Array(props.count)].map((star, i) =>
+						<Star key={i}
+						/>
+					)
+				}
+			</li>
+		</ul>
+	)
+}
